960414ca183fefb9ff8a50022c953547207379d6
Csákány Zsófia <g41i7l@INF.ELTE.HU>
2019-09-25 13:43:32 +0200

    create base app

---
+++ src/main/java/com/alkfejl/pizzaforte/PizzaforteApplication.java
@@ -0,0 +1,13 @@
+package com.alkfejl.pizzaforte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PizzaforteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PizzaforteApplication.class, args);
+	}
+
+}
